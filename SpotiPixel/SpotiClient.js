4322be878e2e42462bb98dad1ecd122942865fa7
ArabianKnight97 <150494024+ArabianKnight97@users.noreply.github.com>
2024-08-14 19:29:55 -0500

    Add files via upload

---
+++ SpotiPixel/SpotiClient.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const config = require('./config');
+
+const apiURL = config.app.PixooUrl;
+const checkSongUrl = `${config.app.SpotiPixelServerUrl}/check-song`;
+let previousSongName = "";
+let lastRequestTime = 0;
+
+async function fetchSongName() {
+  try {
+    const response = await axios.get(checkSongUrl, { timeout: 900 });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching song name:', error);
+    return null;
+  }
+}
+
+async function fetchGif(url) {
+  try {
+    const response = await axios({ url, responseType: 'arraybuffer', timeout: 900 });
+    const gifBuffer = response.data;
+    return { gifBuffer };
+  } catch (error) {
+    console.error('Error downloading GIF:', error);
+    return null;
+  }
+}
+async function playNetGif(gifUrl) {
+  const songName = await fetchSongName();
+  if (songName === previousSongName) {
+    return;
+  }
+  const gifData = await fetchGif(gifUrl);
+  if (!gifData) return;
+  const now = Date.now();
+  const timeSinceLastRequest = now - lastRequestTime;
+  if (timeSinceLastRequest < 5000) {
+    const waitTime = 5000 - timeSinceLastRequest;
+  } else {
+  try {
+    const response = await axios.post(apiURL, {
+      Command: "Device/PlayTFGif",
+      FileType: 2,
+      FileName: gifUrl,
+    }, { timeout: 900 });
+    console.log(response.data);
+    previousSongName = songName;
+    lastRequestTime = Date.now();
+  } catch (error) {
+    console.error('Error sending request to Pixoo API:', error);
+  }
+}
+}
+
+function startGifCheckLoop() {
+  setInterval(() => playNetGif(`${config.app.SpotiPixelServerUrl}/cover.gif`), 1500);
+}
+
+startGifCheckLoop();
---
+++ SpotiPixel/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    app: {
+        clientId: '2d7b7dfff6d748b9ac3e129565cb6adf',
+        clientSecret: '03a69fc2bdbc4c5aa7e5f84dde28e76d',
+        redirectUri: 'https://127.0.0.1/callback',
+        InitPort: 8080,
+        SpotiPixelServerMode: true,
+        SpotiPixelServerPort: 25567,
+        SpotiPixelClientMode: true,
+        PixooUrl: 'http://XXX.XXX.XXX.XXX/post',
+        SpotiPixelServerUrl: 'http://localhost:25567'
+    }
+    };
+    // Copyright © ArtichautDev 2024 All Rights Reserved
